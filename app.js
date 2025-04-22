efc1663f2343bfabca58322d150c09e68143a5d6
github-for-challa <srinivas.challa788@gmail.com>
2025-04-22 08:45:05 -0700

    updated app.js file

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('CI/CD Pipeline Working!'));
+app.get('/', (req, res) => {
+  res.send('✅ CI/CD Pipeline Working Successfully! 🎉\4: CI/CD Pipeline with GitHub Actions & Docker is complete.');
+});
 
-app.listen(port, () => console.log(`App running on port ${port}`));
+app.listen(port, () => {
+  console.log(`🚀 App running on port ${port}`);
+});
+
